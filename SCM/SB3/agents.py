b69a9b39d354f57f1b9651167bc92eaf5d2b6974
adeecc <adeecc11@gmail.com>
2021-07-18 21:40:46 +0530

    Add verbose updates

--- SCM/SB3/agents.py
+++ SCM/SB3/agents.py
@@ -20,9 +20,8 @@ def make_policy(net_architecture=[512, 512]):
     return policy
 
 # Average Reward over 100 episodes: 7041.430871062279 [std. dev: 465.25285975768423]
-
-
 def train_td3(timesteps=5e5, net_architecture=None):
+    print("Created Environment...")
     env = SimpleSupplyChain()
 
     n_actions = env.action_space.shape[-1]
@@ -31,7 +30,7 @@ def train_td3(timesteps=5e5, net_architecture=None):
 
     if net_architecture:
         policy_kwargs = {
-            # "activation_fn": th.nn.ReLU,
+            "activation_fn": th.nn.ReLU,
             "net_arch": net_architecture
         }
     else:
@@ -41,15 +40,18 @@ def train_td3(timesteps=5e5, net_architecture=None):
                 action_noise=action_noise, policy_kwargs=policy_kwargs,
                 verbose=1, tensorboard_log="./tensorboard/TD3")
 
+    print("Starting Model Training...")
     agent.learn(total_timesteps=timesteps, log_interval=10)
 
     file_name = f"td3_{int(time.time())}"
     agent.save(file_name)
 
+    print(f"Training Finished. Model saved as >>> {file_name}")
     return agent
 
 
 def train_ddpg(timesteps=5e5):
+    print("Created Environment...")
     env = SimpleSupplyChain()
 
     n_actions = env.action_space.shape[-1]
@@ -58,11 +60,14 @@ def train_ddpg(timesteps=5e5):
 
     agent = DDPG("MlpPolicy", env, action_noise=action_noise,
                  verbose=1, tensorboard_log="./tensorboard/DDPG")
+
+    print("Starting Model Training...")
     agent.learn(total_timesteps=timesteps, log_interval=10)
 
     file_name = f"ddpg_{int(time.time())}"
     agent.save(file_name)
 
+    print(f"Training Finished. Model saved as >>> {file_name}")
     return agent
 
 
